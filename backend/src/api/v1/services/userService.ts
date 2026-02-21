c3eea9097985c4c6fd0df3d54dc338641e8a434e
Tysen Heck <tysenheck@yahoo.ca>
2026-02-20 22:27:47 -0600

    fixed types

--- backend/src/api/v1/services/userService.ts
+++ backend/src/api/v1/services/userService.ts
@@ -1,6 +1,6 @@
 import db from '../../../database.ts';
 import argon2 from 'argon2';
-
+import { type SafePatients, type Patients } from '../../../../../shared/types.ts'
 
 interface Account {
   email: string;
@@ -8,17 +8,26 @@ interface Account {
 }
 
 export const loginUser = async (user: Account) => {
-  const patient: Account = db.prepare('SELECT * FROM patients WHERE email = ?').get(user.email) as Account;
+  const patient: Patients = db.prepare('SELECT * FROM patients WHERE email = ?').get(user.email) as Patients;
 
   if (!patient) {
     throw new Error('Invalid email or password');
   }
 
 
+
   const isCorrect = await argon2.verify(patient.password, user.password);
+  const safeAccount: SafePatients = {
+    id: patient.id,
+    first_name: patient.first_name,
+    last_name: patient.last_name,
+    email: patient.email,
+    dob: patient.dob,
+    phin: patient.phin
+  }
 
   if (isCorrect) {
-    return patient
+    return safeAccount
   }
   else throw new Error('Invalid email or password');
 }
