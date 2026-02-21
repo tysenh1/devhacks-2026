d4ac7b4e00a395e4b9a1465d5a52e63b8f1cb74a
Liam <lcrosby@rrc.ca>
2026-02-20 21:32:16 -0600

    added register endpoint

--- backend/src/api/v1/services/userService.ts
+++ backend/src/api/v1/services/userService.ts
@@ -1,14 +1,21 @@
 import db from '../../../database.ts';
 import argon2 from 'argon2';
+import { randomUUID } from 'crypto';
 
-
-interface Account {
+interface AccountLogin {
   email: string;
   password: string;
 }
 
-export const loginUser = async (user: Account) => {
-  const patient: Account = db.prepare('SELECT * FROM patients WHERE email = ?').get(user.email) as Account;
+interface Account extends AccountLogin {
+  first_name: string,
+  last_name: string,
+  dob: Date,
+  phin: string,
+}
+
+export const loginUser = async (user: AccountLogin) => {
+  const patient: AccountLogin = db.prepare('SELECT * FROM patients WHERE email = ?').get(user.email) as AccountLogin;
 
   if (!patient) {
     throw new Error('Invalid email or password');
@@ -21,4 +28,24 @@ export const loginUser = async (user: Account) => {
     return patient
   }
   else throw new Error('Invalid email or password');
+}
+
+export const registerUser = async (user: Account) => {
+  try {
+    const stmt = db.prepare(`INSERT INTO patients (id, email, password, first_name, last_name, dob, phin) VALUES (?, ?, ?, ?, ?, ?, ?)`);
+
+    const hashed_password = await argon2.hash(user.password);
+
+    const id = randomUUID()
+
+    const result = stmt.run(id, user.email, hashed_password, user.first_name, user.last_name, user.dob, user.phin);
+
+    if (result.changes == 0) {
+      throw new Error("Failed to insert into table")
+    }
+
+    return user
+  } catch (err) {
+    throw new Error(`${err}`);
+  }
 }
