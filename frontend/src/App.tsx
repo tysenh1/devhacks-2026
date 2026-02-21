8dba80a62e134e6edcf96007b3cc8b15a7ddb35a
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2026-02-21 11:13:35 -0600

    Added vaccine information page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import PatientInformation from "./pages/patient_profile";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import VaccineInformation from "./pages/vaccine_info";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="profile" element={<PatientInformation />} />
+        <Route path="vaccine-info" element={<VaccineInformation />} />
       </Routes>
     </BrowserRouter>
   );
