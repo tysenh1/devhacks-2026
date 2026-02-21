a8032d9298b5a88e0414a614bd5c5204f87b0061
David Chan <dchan51@rrc.ca>
2026-02-20 21:52:42 -0600

    added register pages.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import "./App.css";
-import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
