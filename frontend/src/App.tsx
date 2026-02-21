69b448e29eb8cbc789bb0652bb98e7d557389d80
Allendale Nato <anato2@academic.rrc.ca>
2026-02-21 01:30:58 -0600

    Add patient profile components and integrate into routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="patient" element={<PatientInformation />} />
       </Routes>
     </BrowserRouter>
   );
